a054fd9571237d72f81504dfd61df804473134c0
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-05-01 14:14:16 -0300

    Models Feitas

--- app/Models/Perfume.php
+++ app/Models/Perfume.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Perfume extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo(Categoria::class, 'category_id');
+    }
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'price',
+        'promo',
+        'quantity',
+    ];
 }
