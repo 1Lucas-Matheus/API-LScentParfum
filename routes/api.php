0abdf8916c8db0bf6ba16f1a0ef5e91180263a77
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-05-01 13:58:56 -0300

    Rotas e Controller Criados

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,26 @@
-    <?php
+<?php
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\AuthController;
-    use App\Http\Controllers\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PerfumeController;
+use App\Http\Controllers\CupomController;
+use App\Http\Controllers\LembreteController;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth')->get('/user', function (Request $request) {
+    return response()->json($request->user());
+});
 
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::apiResource('categorias', CategoriaController::class);
+    Route::apiResource('perfumes', PerfumeController::class);
+    Route::apiResource('cupons', CupomController::class);
+    Route::apiResource('lembretes', LembreteController::class);
 
-    Route::middleware('auth')->get('/user', function (Request $request) {
-        return response()->json($request->user());
-    });
+});
