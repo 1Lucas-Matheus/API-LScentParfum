6c2f24c4e485f65fe36ca85a328d7d4d80d75238
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-05-01 22:56:52 -0300

    Rotas Atualizadas

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,11 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth')->get('/user', function (Request $request) {
-    return response()->json($request->user());
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return response()->json($request->user());
+// });
 
-//Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('categorias', CategoriaController::class);
-    Route::apiResource('perfumes', PerfumeController::class);
-    Route::apiResource('cupons', CupomController::class);
-    Route::apiResource('lembretes', LembreteController::class);
-//});
+Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('perfumes', PerfumeController::class);
+Route::apiResource('cupons', CupomController::class);
+Route::apiResource('lembretes', LembreteController::class);
